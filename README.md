ed0cb1819bef7cfa1d2b8e04686dbb8714433bd3
Andrew Dunai <andunai@gmail.com>
2017-04-25 16:25:54 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,45 @@
+# Convensul
+
+A lightweight bash version of the "envconsul" utility.
+
+# Dependencies
+
+- Core utils (`curl`, `awk` and `cut`)
+
+# Usage
+
+Imaging you have this kind of document in Consul KV storage:
+
+    # KV: /config/app
+    FOO: bar
+    SPAM: eggs
+
+Let's create a simple Python app that uses env variables:
+
+    # application.py
+    import os
+
+    print('Foo is %s.' % os.environ['FOO'])
+    print('Spam is best served with %s.' % os.environ['SPAM'])
+
+Now let's run `convensul`:
+
+    $ convensul.sh 127.0.0.1:8500 secret-acl-token /config/app python application.py
+    Foo is bar.
+    Spam is best server with eggs.
+
+You can also store the ACL token in the environment if you don't want to expose its value,
+for example, if your script is used in some sort of CI, e. g. Jenkins.
+
+For this you need to set CONVENSUL_TOKEN env var to your token value and pass `-` instead of the token:
+
+    $ export CONVENSUL_TOKEN=secret-acl-token
+    $ convensul.sh 127.0.0.1:8500 - /config/app python application.py
+    Foo is bar.
+    Spam is best server with eggs.
+
+# Why "convensul"?
+
+Because "convensul" is a mix of "consul" and "env": "conVENsul".
+
+Also it has some sort of "convention" word meaning.
---
+++ convensul.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z $1 || -z $2 || -z $3 || -z $4 ]]
+then
+    echo "Usage: convensul.sh <CONSUL_HOST> <CONSUL_TOKEN> <KV> <APP> [ARG_1] ... [ARG_N]"
+    echo
+    echo "Initializes environment with env vars from Consul YAML document and runs app."
+    echo
+    echo "      CONSUL_HOST       Consul agent address (you can also override port usinng ':')"
+    echo "      CONSUL_TOKEN      Consul ACL token. Set to '-' to use value from CONVENSUL_TOKEN env var."
+    echo "      KV                Path to config document in KV."
+    echo "      APP               Child application to run"
+    echo "      ARG_1 ... ARG_N   Optional arguments to pass to child application"
+    echo
+    echo "Created by Andrew Dunai <a@dun.ai>"
+    exit 1
+fi
+
+HOST=$(echo $1 | awk -F: '{print $1}')
+PORT=$(echo $1 | awk -F: '{print $2}')
+PORT=${PORT:-8500}
+TOKEN=$2
+KV=$3
+ARGS="${@:4}"
+
+if [[ "$TOKEN" == "-" ]]
+then
+    TOKEN=$CONVENSUL_TOKEN
+    if [[ -z "$TOKEN" ]]
+    then
+        echo "Error: CONVENSUL_TOKEN env var must be set if TOKEN is set to '-'."
+        exit 1
+    fi
+fi
+
+DATA=`curl "http://${HOST}:${PORT}/v1/kv/${KV}?dc=dc1&token=${TOKEN}&raw"`
+
+LINES=()
+
+while read -r line
+do
+    key=`echo $line | cut -d: -f1 | xargs`
+    value=`echo $line | cut -d: -f2- | xargs`
+    LINES+=("$key=$value")
+done <<< "$DATA"
+
+env ${LINES[@]} ${ARGS}
